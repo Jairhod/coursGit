2a42e58465bc6dacdd47df31534d7ed28ef48e25
Jairhod <benjair@gmail.com>
2017-03-01 11:37:49 +0100

    back

--- p5-js/Play-01/sketch.js
+++ p5-js/Play-01/sketch.js
@@ -3,8 +3,8 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0, 100);
-  ellipse(mouseX, 50, 100, 100);
+  background(150, 0, 100);
+  ellipse(mouseX, mouseY, 100, 100);
   fill (mouseX, 0, mouseY);
 
 }
